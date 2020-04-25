889ae4736f7286d6aa6c8a2f7693e72a6dda5007
Chris Canal <chris@ocd.codes>
2020-04-26 01:01:57 +0530

    develop: Removes legacy code

--- packages/storybook-typescript/.storybook/manager.js
+++ packages/storybook-typescript/.storybook/manager.js
@@ -1,13 +1,9 @@
 import { addParameters } from '@storybook/react';
 import { themes } from '@storybook/theming';
 import '@storybook/addon-links/register';
-import addons from '@storybook/addons';
-import registerRedux from 'addon-redux/register';
 
 addParameters({
-  options: {
-    theme: themes.dark,
-  },
+	options: {
+		theme: themes.dark,
+	},
 });
-
-registerRedux(addons);
