e9ebdddbf9863370dbc0552ffdc11dcd0551d5f1
Chris Canal <chris@stormideas.com>
2020-04-25 12:56:05 +0530

    Updates

--- precache-manifest.1d9c1ef2c54d552285748a170b2468a1.js
+++ precache-manifest.1d9c1ef2c54d552285748a170b2468a1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc34d2b407004c936b179f84f4f3e135",
+    "revision": "5a3587e8674567b68e11daae7e2a1043",
     "url": "/react-workspaces-playground/index.html"
   },
   {
-    "revision": "6092ca1fd60c41afe691",
+    "revision": "2a6ab373b795c0d68247",
     "url": "/react-workspaces-playground/static/css/main.4fd72fe6.chunk.css"
   },
   {
-    "revision": "cf3e0f16751f51e6938f",
-    "url": "/react-workspaces-playground/static/js/2.5c30abf0.chunk.js"
+    "revision": "90755ff2dadb10683c02",
+    "url": "/react-workspaces-playground/static/js/2.7293e8d6.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/react-workspaces-playground/static/js/2.5c30abf0.chunk.js.LICENSE"
+    "url": "/react-workspaces-playground/static/js/2.7293e8d6.chunk.js.LICENSE"
   },
   {
-    "revision": "6092ca1fd60c41afe691",
-    "url": "/react-workspaces-playground/static/js/main.0a68626c.chunk.js"
+    "revision": "2a6ab373b795c0d68247",
+    "url": "/react-workspaces-playground/static/js/main.3dafead1.chunk.js"
   },
   {
-    "revision": "1c190390830e45b21dae",
-    "url": "/react-workspaces-playground/static/js/runtime-main.39e0e1fd.js"
+    "revision": "831ce3ab441ccb952953",
+    "url": "/react-workspaces-playground/static/js/runtime-main.1887b065.js"
   },
   {
     "revision": "2c813d88489d34f9b9f954e2fac67f5d",
