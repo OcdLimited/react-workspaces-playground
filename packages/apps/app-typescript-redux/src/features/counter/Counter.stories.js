58d2bd091bcb81199d57f14502140daf7f46fb7a
Chris Canal <chris@ocd.codes>
2020-04-26 14:46:01 +0530

    storybook-addons: Adds some more storybook addons

--- packages/apps/app-typescript-redux/src/features/counter/Counter.stories.js
+++ packages/apps/app-typescript-redux/src/features/counter/Counter.stories.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Counter } from './Counter';
 import { Provider } from 'react-redux';
-import { store } from '../../app/store';
 import addons from '@storybook/addons';
 import withRedux from 'addon-redux/withRedux';
+
+import { Counter } from './Counter';
+import { increment, decrement, incrementByAmount } from './counterSlice';
+import { store } from '../../app/store';
 
 const withReduxSettings = {
 	Provider,
 	store,
+	actions: [
+		{
+			name: 'increment',
+			action: increment(),
+		},
+		{
+			name: 'decrement',
+			action: decrement(),
+		},
+		{
+			name: 'incrementByAmount(100)',
+			action: incrementByAmount(100),
+		},
+		{
+			name: 'incrementByAmount(-100)',
+			action: incrementByAmount(-100),
+		},
+	],
 };
 const withReduxDecorator = withRedux(addons)(withReduxSettings);
 
